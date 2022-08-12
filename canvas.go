88130123479874b99bb4aec11158fc8870977690
Behm, John <john.behm@bertelsmann.de>
2022-08-12 20:26:18 +0200

    add buffering and don't paint every change, only paint at most every 3 seconds

--- canvas.go
+++ canvas.go
@@ -5,22 +5,37 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"time"
 )
 
 type Canvas struct {
+	lastTimePainted time.Time
+	buffer          string
 }
 
 func (c *Canvas) Close() {
+	// flush last state
+	c.Flush()
 }
 
 func NewCanvas() *Canvas {
-	c := &Canvas{}
+	c := &Canvas{
+		lastTimePainted: time.Now(),
+	}
 	return c
 }
 
 func (c *Canvas) Paint(s string) {
+	c.buffer = s
+	if time.Since(c.lastTimePainted) > 3*time.Second {
+		clearTerminal()
+		fmt.Println(s)
+	}
+}
+
+func (c *Canvas) Flush() {
 	clearTerminal()
-	fmt.Println(s)
+	fmt.Println(c.buffer)
 }
 
 func clearTerminal() {
